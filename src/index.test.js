9a6c57dd7f300bef9e135181ffbbac2bfbaf5bce
Ivan Huang <ivan.huang@letote.cn>
2020-01-22 17:41:11 +0800

    replace foot leteral string with constant in spec

--- src/index.test.js
+++ src/index.test.js
@@ -2,31 +2,31 @@ import { Length } from './index'
 
 describe('Length', () => {
   it("should 1 'foot' equals 1 'foot'", () => {
-    const length = new Length(1, 'f')
+    const length = new Length(1, Length.FOOT)
 
     expect(length.getValue()).toEqual(1)
-    expect(length.getUnit()).toEqual('f')
+    expect(length.getUnit()).toEqual(Length.FOOT)
   })
 
   it("should 1 'foot' equals 12 inches", () => {
-    const result = new Length(1, 'f').parseTo('inch')
+    const result = new Length(1, Length.FOOT).parseTo('inch')
 
     expect(result.getValue()).toEqual(12)
     expect(result.getUnit()).toEqual('inch')
   })
 
   it("should 3 feet equals 1 'yard'", () => {
-    const result = new Length(3, 'f').parseTo(Length.YARD)
+    const result = new Length(3, Length.FOOT).parseTo(Length.YARD)
 
     expect(result.getValue()).toEqual(1)
     expect(result.getUnit()).toEqual(Length.YARD)
   })
 
   it("should 1 'yard' equals 3 feet", () => {
-    const result = new Length(1, Length.YARD).parseTo('f')
+    const result = new Length(1, Length.YARD).parseTo(Length.FOOT)
 
     expect(result.getValue()).toEqual(3)
-    expect(result.getUnit()).toEqual('f')
+    expect(result.getUnit()).toEqual(Length.FOOT)
   })
 
   it("should 1 'yard' equals 36 inches", () => {
@@ -44,10 +44,10 @@ describe('Length', () => {
   })
 
   it("should 12 inches equals 1 'foot'", () => {
-    const result = new Length(12, 'inch').parseTo('f')
+    const result = new Length(12, 'inch').parseTo(Length.FOOT)
 
     expect(result.getValue()).toEqual(1)
-    expect(result.getUnit()).toEqual('f')
+    expect(result.getUnit()).toEqual(Length.FOOT)
   })
 
   it("should 36 inches equals 1 'yard'", () => {
